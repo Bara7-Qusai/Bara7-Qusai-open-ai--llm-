4d42934ed7b8a197e8572b83aad3ccba39e048ac
Barah Qusai <100060234+Bara7-Qusai@users.noreply.github.com>
2025-09-20 13:25:08 +0200

    Update 6- Generating a product description.py

--- 6- Generating a product description.py
+++ 6- Generating a product description.py
@@ -16,7 +16,7 @@ response = client.chat.completions.create(
     messages=[{"role": "user", "content": prompt}],
     # Experiment with max_completion_tokens and temperature settings
     max_completion_tokens=100,
-    temperature=2
+    temperature=0
 )
 
 print(response.choices[0].message.content)
